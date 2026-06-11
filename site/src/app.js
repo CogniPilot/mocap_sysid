df7bc9de82ca18cbb845bbefc5eda5afd7d856d9
James Goppert <james.goppert@gmail.com>
2026-06-11 15:42:19 -0400

    Flip the displayed rudder deflection sign
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- site/src/app.js
+++ site/src/app.js
@@ -1292,7 +1292,7 @@ function updateAircraftControls(aircraft, controls, deltaS) {
     setHinge(parts.leftAileron, parts.leftAileron?.userData.hingeAxis || SPIN_Z, -1.2 * aileron);
     setHinge(parts.rightAileron, parts.rightAileron?.userData.hingeAxis || SPIN_Z, 1.2 * aileron);
     setHinge(parts.elevator, SPIN_Z, -1.4 * elevator);
-    setHinge(parts.rudder, HINGE_Y, 1.4 * rudder);
+    setHinge(parts.rudder, HINGE_Y, -1.4 * rudder);
     if (parts.prop && parts.prop.userData.baseQuat) {
       parts.prop.userData.spin = (parts.prop.userData.spin || 0) + deltaS * (22 + 90 * thrust);
       setHinge(parts.prop, HINGE_X, parts.prop.userData.spin);
